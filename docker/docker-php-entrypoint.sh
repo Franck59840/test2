0d8f3e81c8ddf453390164a187893c91862834b1
Franck59840 <depoorter.franck@gmail.com>
2023-04-25 11:52:06 +0200

    add command logs and cache

--- docker/docker-php-entrypoint.sh
+++ docker/docker-php-entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+# Create logs and cache directories
+mkdir -p var/log
+chmod -R 777 var/log
+mkdir -p var/cache
+chmod -R 777 var/cache
+
+
 # Install dependencies with Composer
 composer install --no-progress --no-interaction --no-suggest --no-scripts --prefer-dist
 
